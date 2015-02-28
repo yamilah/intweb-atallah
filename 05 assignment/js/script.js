4ca770e2b468c41bcfb338c0263dabacfd59e395
Yamilah Atallah <yami@yamilah.com>
2015-02-28 10:13:25 -0500

    Removed unnecessary CSS, implemented red border when checks fail using JS.

--- 05 assignment/js/script.js
+++ 05 assignment/js/script.js
@@ -1,8 +1,59 @@
-var validation = function() {
-    console.log("oh my god its chicken nuggets");
-}
+document.getElementById("submit").onclick = function() {    
+    var fname = document.getElementsByName("fname")[0];
+    
+    if (fname.value.length <= 2) {
+        fname.classList.add("error");
+    }
+    
+    var lname = document.getElementsByName("lname")[0];
+    
+    if (lname.value.length <= 2) {
+        lname.classList.add("error");
+    }
+    
+    /*var email = document.getElementsByName("email")[0];
+    
+    if (fname.value.length <= 2) {
+        fname.classList.add("error");
+    }*/
+    
+    var uname = document.getElementsByName("uname")[0];
+    
+    if (uname.value.length <= 7) {
+        uname.classList.add("error");
+    }
+    
+    var pass = document.getElementsByName("pass")[0];
+    
+    if (pass.value.length <= 8 || !pass.value.match(/\d+/)) {
+        pass.classList.add("error");
+    }
+    
+    /*var vpass = document.getElementsByName("vpass")[0];
+    
+    if (vpass.value.length <= 2) {
+        vpass.classList.add("error");
+    }
+    
+    var color = document.getElementsByName("fname")[0];
+    
+    if (fname.value.length <= 2) {
+        fname.classList.add("error");
+    }*/
+    
+    return false;
+};
 
-var clear = function() {
-    console.log("oh my god its chicken nuggets");
-}
-
+document.getElementById("reset").onclick = function() {    
+    var response = confirm("Are you sure you want to clear? This will clear all of your fields.");
+    
+    if (response) {
+        var list = document.getElementsByClassName("fields");
+        
+        for (var listIndex = 0; listIndex < list.length; listIndex++) {
+            list[listIndex].value = "";
+        }
+    }
+    
+    return false;
+};
